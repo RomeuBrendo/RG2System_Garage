4a4fddc336f0b318b02c68c6c46148910dc6d2ff
Romeu Brendo <romeu.gomes@lbc.com.br>
2020-08-16 17:58:24 -0300

    Commite depedenciaas

--- RG2System_Garage.Viwer/Program.cs
+++ RG2System_Garage.Viwer/Program.cs
@@ -16,7 +16,7 @@ namespace RG2System_Garage.Viwer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+          //  Application.Run(new Form1());
         }
     }
 }
