072b200e211549c761bc0ae10d1763a7d0893125
LkSayajjym <r.guilhermeamorim@gmail.com>
2020-08-16 20:33:00 -0300

    Commite inicial frmPrincipal

--- RG2System_Garage.Viwer/Formulario/frmPrincipal.Designer.cs
+++ RG2System_Garage.Viwer/Formulario/frmPrincipal.Designer.cs
@@ -28,19 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.btnCadastro = new System.Windows.Forms.Button();
+            this.btnLancamento = new System.Windows.Forms.Button();
+            this.btnConfiguracao = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tmMenu = new System.Windows.Forms.Timer(this.components);
+            this.pnCadastro = new System.Windows.Forms.Panel();
+            this.btnCadCliente = new System.Windows.Forms.Button();
+            this.btnCadProduto = new System.Windows.Forms.Button();
+            this.btnCadVeiculo = new System.Windows.Forms.Button();
+            this.pnLancamentos = new System.Windows.Forms.Panel();
+            this.btnLancServico = new System.Windows.Forms.Button();
+            this.pnConfiguracoes = new System.Windows.Forms.Panel();
+            this.btnConfigGeral = new System.Windows.Forms.Button();
+            this.pnMenu.SuspendLayout();
+            this.pnCadastro.SuspendLayout();
+            this.pnLancamentos.SuspendLayout();
+            this.pnConfiguracoes.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pnMenu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnMenu.BackgroundImage")));
+            this.pnMenu.Controls.Add(this.pnConfiguracoes);
+            this.pnMenu.Controls.Add(this.pnLancamentos);
+            this.pnMenu.Controls.Add(this.pnCadastro);
+            this.pnMenu.Controls.Add(this.label1);
+            this.pnMenu.Controls.Add(this.btnConfiguracao);
+            this.pnMenu.Controls.Add(this.btnLancamento);
+            this.pnMenu.Controls.Add(this.btnCadastro);
+            this.pnMenu.Location = new System.Drawing.Point(0, 4);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(368, 717);
+            this.pnMenu.TabIndex = 0;
+            // 
+            // btnCadastro
+            // 
+            this.btnCadastro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(173)))), ((int)(((byte)(218)))));
+            this.btnCadastro.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCadastro.FlatAppearance.BorderSize = 0;
+            this.btnCadastro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastro.Font = new System.Drawing.Font("Baloo Tamma 2", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastro.ForeColor = System.Drawing.Color.White;
+            this.btnCadastro.Location = new System.Drawing.Point(1, 219);
+            this.btnCadastro.Name = "btnCadastro";
+            this.btnCadastro.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnCadastro.Size = new System.Drawing.Size(364, 40);
+            this.btnCadastro.TabIndex = 0;
+            this.btnCadastro.Text = "CADASTRO";
+            this.btnCadastro.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCadastro.UseVisualStyleBackColor = false;
+            this.btnCadastro.Click += new System.EventHandler(this.btnCadastro_Click);
+            // 
+            // btnLancamento
+            // 
+            this.btnLancamento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(173)))), ((int)(((byte)(218)))));
+            this.btnLancamento.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLancamento.FlatAppearance.BorderSize = 0;
+            this.btnLancamento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLancamento.Font = new System.Drawing.Font("Baloo Tamma 2", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLancamento.ForeColor = System.Drawing.Color.White;
+            this.btnLancamento.Location = new System.Drawing.Point(1, 378);
+            this.btnLancamento.Name = "btnLancamento";
+            this.btnLancamento.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnLancamento.Size = new System.Drawing.Size(364, 40);
+            this.btnLancamento.TabIndex = 1;
+            this.btnLancamento.Text = "LANÇAMENTOS";
+            this.btnLancamento.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLancamento.UseVisualStyleBackColor = false;
+            this.btnLancamento.Click += new System.EventHandler(this.btnLancamento_Click);
+            // 
+            // btnConfiguracao
+            // 
+            this.btnConfiguracao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(173)))), ((int)(((byte)(218)))));
+            this.btnConfiguracao.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConfiguracao.FlatAppearance.BorderSize = 0;
+            this.btnConfiguracao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfiguracao.Font = new System.Drawing.Font("Baloo Tamma 2", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfiguracao.ForeColor = System.Drawing.Color.White;
+            this.btnConfiguracao.Location = new System.Drawing.Point(1, 458);
+            this.btnConfiguracao.Name = "btnConfiguracao";
+            this.btnConfiguracao.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnConfiguracao.Size = new System.Drawing.Size(364, 40);
+            this.btnConfiguracao.TabIndex = 2;
+            this.btnConfiguracao.Text = "CONFIGURAÇÕES";
+            this.btnConfiguracao.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConfiguracao.UseVisualStyleBackColor = false;
+            this.btnConfiguracao.Click += new System.EventHandler(this.btnConfiguracao_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(173)))), ((int)(((byte)(218)))));
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(305, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 35);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tmMenu
+            // 
+            this.tmMenu.Interval = 5;
+            this.tmMenu.Tick += new System.EventHandler(this.tmMenu_Tick);
+            // 
+            // pnCadastro
+            // 
+            this.pnCadastro.Controls.Add(this.btnCadVeiculo);
+            this.pnCadastro.Controls.Add(this.btnCadProduto);
+            this.pnCadastro.Controls.Add(this.btnCadCliente);
+            this.pnCadastro.Location = new System.Drawing.Point(0, 259);
+            this.pnCadastro.Name = "pnCadastro";
+            this.pnCadastro.Size = new System.Drawing.Size(368, 119);
+            this.pnCadastro.TabIndex = 4;
+            // 
+            // btnCadCliente
+            // 
+            this.btnCadCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(194)))), ((int)(((byte)(217)))));
+            this.btnCadCliente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCadCliente.FlatAppearance.BorderSize = 0;
+            this.btnCadCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadCliente.Font = new System.Drawing.Font("Baloo Tamma 2", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadCliente.ForeColor = System.Drawing.Color.White;
+            this.btnCadCliente.Location = new System.Drawing.Point(0, 0);
+            this.btnCadCliente.Name = "btnCadCliente";
+            this.btnCadCliente.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnCadCliente.Size = new System.Drawing.Size(368, 40);
+            this.btnCadCliente.TabIndex = 5;
+            this.btnCadCliente.Text = "CLIENTE";
+            this.btnCadCliente.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCadCliente.UseVisualStyleBackColor = false;
+            // 
+            // btnCadProduto
+            // 
+            this.btnCadProduto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(194)))), ((int)(((byte)(217)))));
+            this.btnCadProduto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCadProduto.FlatAppearance.BorderSize = 0;
+            this.btnCadProduto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadProduto.Font = new System.Drawing.Font("Baloo Tamma 2", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadProduto.ForeColor = System.Drawing.Color.White;
+            this.btnCadProduto.Location = new System.Drawing.Point(0, 40);
+            this.btnCadProduto.Name = "btnCadProduto";
+            this.btnCadProduto.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnCadProduto.Size = new System.Drawing.Size(368, 40);
+            this.btnCadProduto.TabIndex = 6;
+            this.btnCadProduto.Text = "PRODUTO";
+            this.btnCadProduto.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCadProduto.UseVisualStyleBackColor = false;
+            // 
+            // btnCadVeiculo
+            // 
+            this.btnCadVeiculo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(194)))), ((int)(((byte)(217)))));
+            this.btnCadVeiculo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCadVeiculo.FlatAppearance.BorderSize = 0;
+            this.btnCadVeiculo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadVeiculo.Font = new System.Drawing.Font("Baloo Tamma 2", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadVeiculo.ForeColor = System.Drawing.Color.White;
+            this.btnCadVeiculo.Location = new System.Drawing.Point(0, 80);
+            this.btnCadVeiculo.Name = "btnCadVeiculo";
+            this.btnCadVeiculo.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnCadVeiculo.Size = new System.Drawing.Size(368, 40);
+            this.btnCadVeiculo.TabIndex = 7;
+            this.btnCadVeiculo.Text = "VEICULO";
+            this.btnCadVeiculo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCadVeiculo.UseVisualStyleBackColor = false;
+            // 
+            // pnLancamentos
+            // 
+            this.pnLancamentos.Controls.Add(this.btnLancServico);
+            this.pnLancamentos.Location = new System.Drawing.Point(-1, 418);
+            this.pnLancamentos.Name = "pnLancamentos";
+            this.pnLancamentos.Size = new System.Drawing.Size(368, 40);
+            this.pnLancamentos.TabIndex = 8;
+            // 
+            // btnLancServico
+            // 
+            this.btnLancServico.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(194)))), ((int)(((byte)(217)))));
+            this.btnLancServico.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLancServico.FlatAppearance.BorderSize = 0;
+            this.btnLancServico.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLancServico.Font = new System.Drawing.Font("Baloo Tamma 2", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLancServico.ForeColor = System.Drawing.Color.White;
+            this.btnLancServico.Location = new System.Drawing.Point(0, 0);
+            this.btnLancServico.Name = "btnLancServico";
+            this.btnLancServico.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnLancServico.Size = new System.Drawing.Size(368, 40);
+            this.btnLancServico.TabIndex = 5;
+            this.btnLancServico.Text = "PRODUTOS/SERVIÇOS";
+            this.btnLancServico.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLancServico.UseVisualStyleBackColor = false;
+            // 
+            // pnConfiguracoes
+            // 
+            this.pnConfiguracoes.Controls.Add(this.btnConfigGeral);
+            this.pnConfiguracoes.Location = new System.Drawing.Point(-1, 498);
+            this.pnConfiguracoes.Name = "pnConfiguracoes";
+            this.pnConfiguracoes.Size = new System.Drawing.Size(368, 40);
+            this.pnConfiguracoes.TabIndex = 9;
+            // 
+            // btnConfigGeral
+            // 
+            this.btnConfigGeral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(194)))), ((int)(((byte)(217)))));
+            this.btnConfigGeral.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConfigGeral.FlatAppearance.BorderSize = 0;
+            this.btnConfigGeral.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfigGeral.Font = new System.Drawing.Font("Baloo Tamma 2", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfigGeral.ForeColor = System.Drawing.Color.White;
+            this.btnConfigGeral.Location = new System.Drawing.Point(0, 0);
+            this.btnConfigGeral.Name = "btnConfigGeral";
+            this.btnConfigGeral.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnConfigGeral.Size = new System.Drawing.Size(368, 40);
+            this.btnConfigGeral.TabIndex = 5;
+            this.btnConfigGeral.Text = "GERAL";
+            this.btnConfigGeral.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConfigGeral.UseVisualStyleBackColor = false;
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1151, 719);
+            this.Controls.Add(this.pnMenu);
+            this.KeyPreview = true;
             this.Name = "frmPrincipal";
-            this.Text = "frmPrincipal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnMenu.ResumeLayout(false);
+            this.pnCadastro.ResumeLayout(false);
+            this.pnLancamentos.ResumeLayout(false);
+            this.pnConfiguracoes.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.Button btnConfiguracao;
+        private System.Windows.Forms.Button btnLancamento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer tmMenu;
+        private System.Windows.Forms.Panel pnLancamentos;
+        private System.Windows.Forms.Button btnLancServico;
+        private System.Windows.Forms.Panel pnCadastro;
+        private System.Windows.Forms.Button btnCadVeiculo;
+        private System.Windows.Forms.Button btnCadProduto;
+        private System.Windows.Forms.Button btnCadCliente;
+        private System.Windows.Forms.Panel pnConfiguracoes;
+        private System.Windows.Forms.Button btnConfigGeral;
     }
 }
--- RG2System_Garage.Viwer/Formulario/frmPrincipal.cs
+++ RG2System_Garage.Viwer/Formulario/frmPrincipal.cs
@@ -12,9 +12,108 @@ namespace RG2System_Garage.Viwer.Formulario
 {
     public partial class frmPrincipal : MetroFramework.Forms.MetroForm
     {
+
+        bool glb_HideMenu;
+
         public frmPrincipal()
         {
             InitializeComponent();
+            pnMenu.Left = -304;
+            glb_HideMenu = true;
+            HideAllMenu();
+        }
+
+        private void tmMenu_Tick(object sender, EventArgs e)
+        {
+            if (glb_HideMenu)
+            {
+                label1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+                pnMenu.Left += 16;
+                if (pnMenu.Left == 0)
+                {
+                    glb_HideMenu = false;
+                    this.Refresh();
+                    tmMenu.Stop();
+                }
+            }
+            else
+            {
+                HideAllMenu();
+                label1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pnMenu.Left -= 16;
+                if (pnMenu.Left == -304)
+                {
+                    glb_HideMenu = true;
+                    this.Refresh();
+                    tmMenu.Stop();
+
+                }
+            }
+        }
+
+        public void HideAllMenu()
+        {
+            btnCadastro.Top = 219;
+            btnLancamento.Top = 219 + btnCadastro.Height;
+            btnConfiguracao.Top = btnLancamento.Top + btnLancamento.Height;
+            pnCadastro.Visible = false;
+            pnLancamentos.Visible = false;
+            pnConfiguracoes.Visible = false;
+        }
+
+        public void ShowSubMenu(Panel Painel)
+        {
+            HideAllMenu();
+            if (Painel.Name == "pnCadastro")
+            {
+                btnLancamento.Top = btnLancamento.Top + pnCadastro.Height;
+                btnConfiguracao.Top = btnConfiguracao.Top + pnCadastro.Height;
+                Painel.Top = 219 + btnCadastro.Height;
+                Painel.Visible = true;
+            }
+            if (Painel.Name == "pnLancamentos")
+            {
+                btnConfiguracao.Top = btnConfiguracao.Top + pnLancamentos.Height;
+                Painel.Top = btnLancamento.Top + btnLancamento.Height;
+                Painel.Visible = true;
+            }
+            if (Painel.Name == "pnConfiguracoes")
+            {
+                Painel.Top = btnConfiguracao.Top + btnConfiguracao.Height;
+                Painel.Visible = true;
+            }
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            tmMenu.Start();
+        }
+
+        private void btnCadastro_Click(object sender, EventArgs e)
+        {
+            if (pnCadastro.Visible)
+                HideAllMenu();
+            else
+                ShowSubMenu(pnCadastro); 
+        }
+
+        private void btnLancamento_Click(object sender, EventArgs e)
+        {
+            if (pnLancamentos.Visible)
+                HideAllMenu();
+            else
+                ShowSubMenu(pnLancamentos);
+        }
+
+        private void btnConfiguracao_Click(object sender, EventArgs e)
+        {
+            if (pnConfiguracoes.Visible)
+                HideAllMenu();
+            else
+                ShowSubMenu(pnConfiguracoes);
         }
     }
 }
