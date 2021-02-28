8282fc692dbe4f98e11d5dca3414cea7d6a2d982
RomeuBrendo <37197350+RomeuBrendo@users.noreply.github.com>
2021-02-28 17:03:33 -0300

    Commite parcial OR
    Commite parcial OR

--- RG2System_Garage.Domain/Commands/OrdemServico/ORPagamentoResponse.cs
+++ RG2System_Garage.Domain/Commands/OrdemServico/ORPagamentoResponse.cs
@@ -5,6 +5,7 @@ namespace RG2System_Garage.Domain.Commands.OrdemServico
     public class ORPagamentoResponse
     {
         public Guid FormaPagamentoId { get; set; }
+        public string DescricaoFormaPgt { get; set; }
         public Guid OrdemServicoId { get; set; }
         public string Valor { get; set; }
         public string Descricao { get; set; } //apenas p/transferencia de grid
